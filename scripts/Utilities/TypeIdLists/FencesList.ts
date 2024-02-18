958b3b41aeaa1f7469ea591229487db16dec1919
Cole Green <17110935+nox7@users.noreply.github.com>
2024-02-18 13:52:23 -0600

    Add type Id lists

---
+++ scripts/Utilities/TypeIdLists/FencesList.ts
@@ -0,0 +1,13 @@
+export default [
+    "minecraft:oak_fence",
+    "minecraft:spruce_fence",
+    "minecraft:birch_fence",
+    "minecraft:jungle_fence",
+    "minecraft:acacia_fence",
+    "minecraft:dark_oak_fence",
+    "minecraft:mangrove_fence",
+    "minecraft:cherry_fence",
+    "minecraft:bamboo_fence",
+    "minecraft:crimson_fence",
+    "vwarped_fence",
+];
---
+++ scripts/Utilities/TypeIdLists/WallsList.ts
@@ -0,0 +1,7 @@
+export default [
+    "minecraft:cobblestone_wall",
+    "minecraft:cobbled_deepslate_wall",
+    "minecraft:polished_deepslate_wall",
+    "minecraft:deepslate_brick_wall",
+    "minecraft:deepslate_tile_wall",
+];
