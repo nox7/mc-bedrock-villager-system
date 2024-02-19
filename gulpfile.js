765d538857bd407c7d62ea58a1e57edd16428356
Cole Green <17110935+nox7@users.noreply.github.com>
2024-02-19 12:49:15 -0600

    Include JS in Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -48,7 +48,7 @@ const copy_content = gulp.parallel(copy_behavior_packs, copy_resource_packs);
 
 function compile_scripts() {
   return gulp
-    .src("scripts/**/*.ts")
+    .src(["scripts/**/*.ts", "scripts/**/*.js"])
     .pipe(sourcemaps.init())
     .pipe(
       ts({
@@ -58,6 +58,7 @@ function compile_scripts() {
         strict: true,
         target: "es2020",
         noImplicitAny: true,
+        allowJs: true,
       })
     )
     .pipe(
