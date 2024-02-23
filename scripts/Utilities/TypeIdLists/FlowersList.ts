f03456d45b11c39e87831bf1e2746f7ef6c8b445
Cole Green <17110935+nox7@users.noreply.github.com>
2024-02-22 19:16:50 -0600

    Create FlowersList.ts

---
+++ scripts/Utilities/TypeIdLists/FlowersList.ts
@@ -0,0 +1,8 @@
+export default [
+    "minecraft:yellow_flower",
+    "minecraft:red_flower",
+    "minecraft:double_plant",
+    "minecraft:torchflower",
+    "minecraft:pitcher_plant",
+    "minecraft:wither_rose",
+];
