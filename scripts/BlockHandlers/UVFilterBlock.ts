7bf377ca566a46db9a3f60a23e4d188a02edc739
Cole Green <17110935+nox7@users.noreply.github.com>
2024-02-25 16:24:05 -0600

    Add UVFilter support for all ground-growable crops

--- scripts/BlockHandlers/UVFilterBlock.ts
+++ scripts/BlockHandlers/UVFilterBlock.ts
@@ -1,20 +1,78 @@
-import { Block, BlockPermutation, BlockRaycastHit, BlockRaycastOptions, Vector3 } from "@minecraft/server";
+import { Block, BlockPermutation, BlockRaycastHit, BlockRaycastOptions, Vector, Vector3 } from "@minecraft/server";
+
+type BlockGrowthStateData = {
+    GrowthStateName: string;
+    GrowthMaxNumericValue: number
+};
+
+type RecognizedBlockTypeAndData = {
+    [key: string]: BlockGrowthStateData
+};
 
 export class UVFilterBlock{
+    private static RecognizedBlocks: RecognizedBlockTypeAndData = {
+        "minecraft:wheat": {
+            "GrowthStateName": "growth",
+            "GrowthMaxNumericValue": 7
+        },
+        "minecraft:potatoes": {
+            "GrowthStateName": "growth",
+            "GrowthMaxNumericValue": 7
+        },
+        "minecraft:carrots": {
+            "GrowthStateName": "growth",
+            "GrowthMaxNumericValue": 7
+        },
+        "minecraft:beetroot": {
+            "GrowthStateName": "growth",
+            "GrowthMaxNumericValue": 7
+        },
+        "minecraft:melon_stem": {
+            "GrowthStateName": "growth",
+            "GrowthMaxNumericValue": 7
+        },
+        "minecraft:pumpkin_stem": {
+            "GrowthStateName": "growth",
+            "GrowthMaxNumericValue": 7
+        },
+        "minecraft:reeds": {
+            "GrowthStateName": "age",
+            "GrowthMaxNumericValue": 15
+        }
+    };
+    
     /**
      * Fires when the block itself ticks
      */
     public static OnBlockTick(block: Block): void{
-        const wheat: Block | null = UVFilterBlock.GetWheatBlockBeneath(block);
-        const growthStage = Number(wheat?.permutation.getState("growth"));
-
-        if (wheat !== null){
-            if (!isNaN(growthStage)){
-                // If the wheat's growth stage is less than the maximum (7)
-                // Increment it
-                if (growthStage < 7){
-                    if (wheat.isValid()){
-                        wheat.setPermutation(wheat.permutation.withState("growth", growthStage + 1));
+        const growableBlock: Block | null = UVFilterBlock.GetRecognizedGrowableBlockBelow(block);
+        
+        if (growableBlock !== null){
+            // We handle sugar cane differently
+            if (growableBlock.typeId === "minecraft:reeds"){
+                const currentHeight: number = UVFilterBlock.GetCurrentSugarCaneHeight(growableBlock);
+                if (currentHeight < 4){
+                    // Check if the block above it is air
+                    let blockAboveGrowableBlock: Block | undefined;
+                    try{
+                        blockAboveGrowableBlock = growableBlock.above(1);
+                    }catch(e){}
+                    if (blockAboveGrowableBlock !== undefined){
+                        if (blockAboveGrowableBlock.isAir){
+                            // Make it a sugar cane
+                            blockAboveGrowableBlock.setPermutation(growableBlock.permutation.withState("age", 0));
+                        }
+                    }
+                }
+            }else{
+                const growthStageData: BlockGrowthStateData = UVFilterBlock.RecognizedBlocks[growableBlock.typeId];
+                const growthStageValue = Number(growableBlock.permutation.getState(growthStageData.GrowthStateName));
+                if (!isNaN(growthStageValue)){
+                    // Check if the growth stage is less than its known maximum
+                    if (growthStageValue < growthStageData.GrowthMaxNumericValue){
+                        if (growableBlock.isValid()){
+                            growableBlock.setPermutation(growableBlock.permutation.withState(growthStageData.GrowthStateName, growthStageValue + 1));
+                        }
                     }
                 }
             }
@@ -24,7 +82,7 @@ export class UVFilterBlock{
     /**
      * Searched 1-10 blocks beneath the provided block for wheat
      */
-    private static GetWheatBlockBeneath(block: Block): Block | null{
+    private static GetRecognizedGrowableBlockBelow(block: Block): Block | null{
         const raycastOptions: BlockRaycastOptions = {
             includeLiquidBlocks: false,
             includePassableBlocks: true,
@@ -33,17 +91,50 @@ export class UVFilterBlock{
         
         const blockCenter: Vector3 = block.center();
         const blockRaycastHit: BlockRaycastHit | undefined = block.dimension.getBlockFromRay(
-            {x: blockCenter.x, y: blockCenter.y - 1, z: blockCenter.z},
+            Vector.subtract(blockCenter, {x: 0, y: 1, z: 0}),
             {x: 0, y:-1, z:0}, 
             raycastOptions
             );
             
         if (blockRaycastHit !== undefined){
-            if (blockRaycastHit.block.typeId === "minecraft:wheat"){
+            const recognizedTypeIds: string[] = Object.keys(UVFilterBlock.RecognizedBlocks);
+            if (recognizedTypeIds.indexOf(blockRaycastHit.block.typeId) > -1){
+                // Found a recognized growable block
                 return blockRaycastHit.block;
             }
         }
 
         return null;
+    }
+
+    /**
+     * Returns the current vertical height of a sugar cane block
+     */
+    private static GetCurrentSugarCaneHeight(sugarCaneBlock: Block): number{
+        const maxHeight: number = 10;
+        let height: number = 1;
+        let nextBlockBelow: Block | undefined;
+        do{
+            try{
+                if (nextBlockBelow === undefined){
+                    nextBlockBelow = sugarCaneBlock.below(1);
+                }else{
+                    nextBlockBelow = nextBlockBelow.below(1);
+                }
+            }catch(e){}
+
+            if (nextBlockBelow !== undefined){
+                if (nextBlockBelow.typeId === "minecraft:reeds"){
+                    ++height;
+                }else{
+                    break;
+                }
+            }else{
+                break;
+            }
+
+        }while(nextBlockBelow !== undefined && height < maxHeight);
+
+        return height;
     }
 }
