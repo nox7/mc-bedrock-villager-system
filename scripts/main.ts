9282b729e15a41e9fb73d4397d5858a9a18d61f7
Cole Green <17110935+nox7@users.noreply.github.com>
2024-03-16 07:29:02 -0500

    Format file

--- scripts/main.ts
+++ scripts/main.ts
@@ -2,7 +2,30 @@
  * NOTE: This main.ts is currently a mess and is not structurted. Code is WIP and will be more class-OOPed or whatever the hell later.
  */
 
-import { world, system, ItemUseOnBeforeEvent, Player, PlayerPlaceBlockAfterEvent, Block, Vector3, Entity, EntityLoadAfterEvent, ScriptEventCommandMessageAfterEvent, EntityEquippableComponent, PlayerInteractWithBlockAfterEvent, EquipmentSlot, BlockPermutation, EntityInventoryComponent, ItemStack, PlayerBreakBlockBeforeEvent, PlayerInteractWithBlockBeforeEvent, ContainerSlot, ChatSendAfterEvent, Dimension, Vector } from "@minecraft/server";
+import {
+  world,
+  system,
+  ItemUseOnBeforeEvent,
+  Player,
+  PlayerPlaceBlockAfterEvent,
+  Block,
+  Vector3,
+  Entity,
+  EntityLoadAfterEvent,
+  ScriptEventCommandMessageAfterEvent,
+  EntityEquippableComponent,
+  PlayerInteractWithBlockAfterEvent,
+  EquipmentSlot,
+  BlockPermutation,
+  EntityInventoryComponent,
+  ItemStack,
+  PlayerBreakBlockBeforeEvent,
+  PlayerInteractWithBlockBeforeEvent,
+  ContainerSlot,
+  ChatSendAfterEvent,
+  Dimension,
+  Vector,
+} from "@minecraft/server";
 import WoodcutterManagerBlock from "./BlockHandlers/WoodcutterManagerBlock.js";
 import Woodcutter from "./NPCs/Woodcutter.js";
 import Debug from "./Debug/Debug.js";
@@ -17,6 +40,9 @@ import { AutoSortActivatorBlock } from "./BlockHandlers/AutoSortActivatorBlock.j
 import { BiomeHelper } from "./Utilities/Biomes/BiomeHelper.js";
 import { RadiusFinder } from "./BlockFinders/RadiusFinder.js";
 import { SingleBedManager } from "./SingleBedManager.js";
+import { FloodFillIteratorOptions } from "./NoxBedrockUtilities/Iterators/FloodFill/FloodFillIIteratorOptions.js";
+import FloodFillIterator from "./NoxBedrockUtilities/Iterators/FloodFill/FloodFillIterator.js";
+import { VectorUtils } from "./NoxBedrockUtilities/Vector/VectorUtils.js";
 
 Debug.LogLevel = LogLevel.All;
 
@@ -25,40 +51,40 @@ system.runInterval(() => {
   npcHandler.OnGameTick();
 });
 
-system.afterEvents.scriptEventReceive.subscribe( (event: ScriptEventCommandMessageAfterEvent) => {
-  if (event.id === "nox:uv-block-tick"){
+system.afterEvents.scriptEventReceive.subscribe((event: ScriptEventCommandMessageAfterEvent) => {
+  if (event.id === "nox:uv-block-tick") {
     system.run(() => {
-      if (event.sourceBlock !== undefined){
-        if (event.sourceBlock.isValid()){
+      if (event.sourceBlock !== undefined) {
+        if (event.sourceBlock.isValid()) {
           UVFilterBlock.OnBlockTick(event.sourceBlock);
         }
       }
-    })
-  }else if (event.id === "nox:open-wine-barrel-step-on"){
-    if (event.sourceBlock !== undefined){
-      if (event.sourceBlock.isValid()){
+    });
+  } else if (event.id === "nox:open-wine-barrel-step-on") {
+    if (event.sourceBlock !== undefined) {
+      if (event.sourceBlock.isValid()) {
         //if (event.sourceEntity !== undefined){
-          OpenWineBarrelBlock.OnBlockSteppedOn(event.sourceBlock);
+        OpenWineBarrelBlock.OnBlockSteppedOn(event.sourceBlock);
         //}
       }
     }
-  }else if (event.id === "nox:closed-wine-barrel-tick"){
-    if (event.sourceBlock !== undefined){
-      if (event.sourceBlock.isValid()){
+  } else if (event.id === "nox:closed-wine-barrel-tick") {
+    if (event.sourceBlock !== undefined) {
+      if (event.sourceBlock.isValid()) {
         ClosedWineBarrelBlock.OnQueuedTicking(event.sourceBlock);
       }
     }
   }
 });
 
-world.afterEvents.playerPlaceBlock.subscribe(async (playerPlaceBlockEvent : PlayerPlaceBlockAfterEvent) => {
+world.afterEvents.playerPlaceBlock.subscribe(async (playerPlaceBlockEvent: PlayerPlaceBlockAfterEvent) => {
   const block = playerPlaceBlockEvent.block;
 
   // Player placed a woodcutter-manager block
-  if (block.permutation.matches("nox:woodcutter-manager")){
+  if (block.permutation.matches("nox:woodcutter-manager")) {
     const woodcutterManagerBlock: WoodcutterManagerBlock = new WoodcutterManagerBlock(block);
     const woodcutterNpc: Woodcutter | null = woodcutterManagerBlock.SpawnWoodcutter(npcHandler);
-    if (woodcutterNpc){
+    if (woodcutterNpc) {
       npcHandler.RegisterNPC(woodcutterNpc);
     }
   }
@@ -66,7 +92,7 @@ world.afterEvents.playerPlaceBlock.subscribe(async (playerPlaceBlockEvent : Play
 
 world.beforeEvents.playerBreakBlock.subscribe(async (event: PlayerBreakBlockBeforeEvent) => {
   const block: Block = event.block;
-  if (block.typeId === "nox:woodcutter-manager"){
+  if (block.typeId === "nox:woodcutter-manager") {
     const blockLocation: Vector3 = block.location;
     system.run(() => {
       Woodcutter.OnWoodcutterManagerBlockBroke(blockLocation);
@@ -74,7 +100,7 @@ world.beforeEvents.playerBreakBlock.subscribe(async (event: PlayerBreakBlockBefo
   }
 });
 
-world.beforeEvents.itemUseOn.subscribe((itemUseOnBeforeEvent : ItemUseOnBeforeEvent) => {
+world.beforeEvents.itemUseOn.subscribe((itemUseOnBeforeEvent: ItemUseOnBeforeEvent) => {
   // if (itemUseOnBeforeEvent.itemStack.typeId === "nox:grapes"){
   //   if (itemUseOnBeforeEvent.block.typeId === "nox:wine-barrel"){
   //     itemUseOnBeforeEvent.cancel = true;
@@ -82,83 +108,85 @@ world.beforeEvents.itemUseOn.subscribe((itemUseOnBeforeEvent : ItemUseOnBeforeEv
   //   }
   // }
 
-  if (itemUseOnBeforeEvent.block.permutation.matches("nox:woodcutter-manager")){
+  if (itemUseOnBeforeEvent.block.permutation.matches("nox:woodcutter-manager")) {
     const player: Player = itemUseOnBeforeEvent.source;
-    if (player.isSneaking){
+    if (player.isSneaking) {
       // Do nothing, let them act normally on this block
       // world.sendMessage("IS SNEAKING");
-    }else{
-      // Cancel whatever the player tried to do on this 
+    } else {
+      // Cancel whatever the player tried to do on this
       // world.sendMessage("Is woodcutter");
       itemUseOnBeforeEvent.cancel = true;
     }
-  }else{
+  } else {
     // world.sendMessage("Is not woodcutter");
   }
 });
 
-world.beforeEvents.playerInteractWithBlock.subscribe((interactEvent : PlayerInteractWithBlockBeforeEvent) => {
+world.beforeEvents.playerInteractWithBlock.subscribe((interactEvent: PlayerInteractWithBlockBeforeEvent) => {
   const player: Player = interactEvent.player;
 
-  if (PlayerDebounceManager.IsDebounced(player, 350)){
+  if (PlayerDebounceManager.IsDebounced(player, 350)) {
     return;
   }
 
   PlayerDebounceManager.Debounce(player);
 
-  let equipmentComponent: EntityEquippableComponent | undefined
+  let equipmentComponent: EntityEquippableComponent | undefined;
   let inventoryComponent: EntityInventoryComponent | undefined;
   let slot: ContainerSlot | undefined;
-  try{
+  try {
     equipmentComponent = player.getComponent(EntityEquippableComponent.componentId);
     inventoryComponent = player.getComponent(EntityInventoryComponent.componentId);
     slot = equipmentComponent?.getEquipmentSlot(EquipmentSlot.Mainhand);
-  }catch(e){
+  } catch (e) {
     return;
   }
 
   const targetBlock = interactEvent.block;
-  if (!targetBlock.isValid()){
+  if (!targetBlock.isValid()) {
     return;
   }
 
   // Handle nox:auto-sorter-activator block
-  if (targetBlock.typeId === "nox:auto-sorter-activator"){
+  if (targetBlock.typeId === "nox:auto-sorter-activator") {
     interactEvent.cancel = true;
     const autoSorter = new AutoSortActivatorBlock(targetBlock);
     system.run(() => {
       autoSorter.OnPlayerInteract(player);
     });
     return;
-  }else if (targetBlock.typeId === "nox:woodcutter-manager"){
+  } else if (targetBlock.typeId === "nox:woodcutter-manager") {
     interactEvent.cancel = true;
     system.run(() => {
       const woodcutterManagerBlock = WoodcutterManagerBlock.GetFromLocation(targetBlock.location);
-      if (woodcutterManagerBlock !== null){
+      if (woodcutterManagerBlock !== null) {
         woodcutterManagerBlock.OnPlayerInteract(player);
       }
     });
     return;
   }
 
-  if (slot !== undefined && slot.isValid() && slot.hasItem()){
-    if (slot.typeId === "nox:grapes"){
-      if (targetBlock.typeId === "nox:wine-barrel"){
+  if (slot !== undefined && slot.isValid() && slot.hasItem()) {
+    if (slot.typeId === "nox:grapes") {
+      if (targetBlock.typeId === "nox:wine-barrel") {
         const grapeCount = targetBlock.permutation.getState("nox:grape-count");
-        if (grapeCount !== undefined){
-          if (Number(grapeCount) < 12){
+        if (grapeCount !== undefined) {
+          if (Number(grapeCount) < 12) {
             interactEvent.cancel = true;
             system.run(() => {
               const slotItemStack = slot?.getItem();
-              if (slotItemStack !== undefined){
-                targetBlock.setPermutation(targetBlock.permutation.withState("nox:grape-count", Number(grapeCount) + 1));
+              if (slotItemStack !== undefined) {
+                targetBlock.setPermutation(
+                  targetBlock.permutation.withState("nox:grape-count", Number(grapeCount) + 1)
+                );
                 player.playSound("item.book.put", {
-                  volume: 0.25
+                  volume: 0.25,
                 });
-                if (slotItemStack.amount > 1){
+                if (slotItemStack.amount > 1) {
                   slotItemStack.amount = slotItemStack.amount - 1;
                   slot?.setItem(slotItemStack);
-                }else{
+                } else {
                   slot?.setItem(undefined);
                 }
               }
@@ -166,10 +194,10 @@ world.beforeEvents.playerInteractWithBlock.subscribe((interactEvent : PlayerInte
           }
         }
       }
-    }else if (slot.typeId === "nox:empty-tankard"){
-      if (targetBlock.typeId === "nox:wine-barrel-finished"){
+    } else if (slot.typeId === "nox:empty-tankard") {
+      if (targetBlock.typeId === "nox:wine-barrel-finished") {
         system.run(() => {
-          if (slot !== undefined && slot?.isValid()){
+          if (slot !== undefined && slot?.isValid()) {
             FinishedWineBarrelBlock.OnEmptyTankardUsedOn(targetBlock, slot, player);
           }
         });
@@ -178,49 +206,51 @@ world.beforeEvents.playerInteractWithBlock.subscribe((interactEvent : PlayerInte
   }
 });
 
-world.afterEvents.playerInteractWithBlock.subscribe((interactEvent : PlayerInteractWithBlockAfterEvent) => {
+world.afterEvents.playerInteractWithBlock.subscribe((interactEvent: PlayerInteractWithBlockAfterEvent) => {
   const player: Player = interactEvent.player;
 
-  let equipmentComponent: EntityEquippableComponent | undefined
+  let equipmentComponent: EntityEquippableComponent | undefined;
   let inventoryComponent: EntityInventoryComponent | undefined;
   let slot: ContainerSlot | undefined;
   const targetBlock: Block = interactEvent.block;
-  try{
+  try {
     equipmentComponent = player.getComponent(EntityEquippableComponent.componentId);
     inventoryComponent = player.getComponent(EntityInventoryComponent.componentId);
     slot = equipmentComponent?.getEquipmentSlot(EquipmentSlot.Mainhand);
-  }catch(e){
+  } catch (e) {
     return;
   }
 
-  if (targetBlock.isValid()){
-    if (targetBlock.typeId === "minecraft:bed"){
+  if (targetBlock.isValid()) {
+    if (targetBlock.typeId === "minecraft:bed") {
       SingleBedManager.OnPlayerInteractWithBed(player);
       return;
     }
   }
 
-  if (slot !== undefined && slot.isValid() && slot.hasItem()){
-    if (slot.typeId === "minecraft:shears"){
-      if (targetBlock.typeId === "nox:grapevine"){
+  if (slot !== undefined && slot.isValid() && slot.hasItem()) {
+    if (slot.typeId === "minecraft:shears") {
+      if (targetBlock.typeId === "nox:grapevine") {
         const grapevineGrowthStage = targetBlock.permutation.getState("nox:growth_stage");
-        if (grapevineGrowthStage === 10){
-          if (inventoryComponent !== undefined){
+        if (grapevineGrowthStage === 10) {
+          if (inventoryComponent !== undefined) {
             // Get the direction
             const currentCardinalDirection = targetBlock.permutation.getState("minecraft:cardinal_direction");
             targetBlock.setPermutation(
-              BlockPermutation
-              .resolve("nox:grapevine")
-              .withState("nox:growth_stage", 7)
-              .withState("minecraft:cardinal_direction", currentCardinalDirection != undefined ? currentCardinalDirection : "north")
-              );
+              BlockPermutation.resolve("nox:grapevine")
+                .withState("nox:growth_stage", 7)
+                .withState(
+                  "minecraft:cardinal_direction",
+                  currentCardinalDirection != undefined ? currentCardinalDirection : "north"
+                )
+            );
             // inventoryComponent.container?.addItem(new ItemStack("nox:grapes", 6));
             // Just spawn the item instead
             const blockCenter: Vector3 = targetBlock.center();
             const spawnLocation: Vector3 = {
               x: blockCenter.x,
               y: blockCenter.y + 0.5,
-              z: blockCenter.z
+              z: blockCenter.z,
             };
             player.dimension.spawnItem(new ItemStack("nox:grapes", 1), spawnLocation);
             player.playSound("mob.sheep.shear");
@@ -231,46 +261,92 @@ world.afterEvents.playerInteractWithBlock.subscribe((interactEvent : PlayerInter
   }
 });
 
-world.afterEvents.worldInitialize.subscribe( () => {
+world.afterEvents.worldInitialize.subscribe(() => {
   // Check if there is a world-scoped dynamic property for the Woodcutter NPC
   const nextWoodCutterId = world.getDynamicProperty("nox:next_woodcutter_id");
-  if (nextWoodCutterId === undefined){
+  if (nextWoodCutterId === undefined) {
     // This property needs to be registered to this world
     world.setDynamicProperty("nox:next_woodcutter_id", 1);
   }
 });
 
-world.afterEvents.entityLoad.subscribe( (e: EntityLoadAfterEvent) => {
+world.afterEvents.entityLoad.subscribe((e: EntityLoadAfterEvent) => {
   const entity: Entity = e.entity;
   // Did we just load a Woodcutter?
-  if (entity.typeId === "nox:woodcutter"){
+  if (entity.typeId === "nox:woodcutter") {
     // If it is not already cached in memory, then this woodcutter needs to be registered on the server
-    if (Woodcutter.GetFromCache(entity) === null){
+    if (Woodcutter.GetFromCache(entity) === null) {
       Woodcutter.LoadFromExistingEntity(entity, npcHandler);
     }
-  }else if (entity.typeId === "minecraft:creeper"){
+  } else if (entity.typeId === "minecraft:creeper") {
     system.runTimeout(() => {
-      if (entity.isValid()){
+      if (entity.isValid()) {
         entity.kill();
       }
     }, 15);
   }
 });
 
-world.afterEvents.chatSend.subscribe( async (e: ChatSendAfterEvent) => {
-  if (e.message === "biome"){
+world.afterEvents.chatSend.subscribe(async (e: ChatSendAfterEvent) => {
+  if (e.message === "biome") {
     system.run(async () => {
       const biomeHelper = new BiomeHelper();
       const biomeType = await biomeHelper.GetBiomeOfLocation(e.sender.location, e.sender.dimension);
       e.sender.sendMessage(String(biomeType?.id));
-    })
-  }else if (e.message === "test"){
+    });
+  } else if (e.message === "test") {
     const blockFinder = new RadiusFinder();
-    const blockFound = await blockFinder.GetBlockInRadius(e.sender.location, "minecraft:oak_log", 5, e.sender.dimension);
-    if (blockFound === undefined){
+    const blockFound = await blockFinder.GetBlockInRadius(
+      e.sender.location,
+      "minecraft:oak_log",
+      5,
+      e.sender.dimension
+    );
+    if (blockFound === undefined) {
       e.sender.sendMessage("Not found");
-    }else{
+    } else {
       e.sender.sendMessage("Found: " + blockFound.typeId);
+    }
+  } else if (e.message === "find-wood") {
+    console.warn("Finding oak log within 64^3 block cuboid.");
+    const startLocation: Vector3 = e.sender.location;
+    const dimension: Dimension = world.getDimension("overworld");
+    const maxDistance: number = 64; // Search within 15 blocks
+    const floodFillOptions = new FloodFillIteratorOptions(startLocation, dimension, maxDistance);
+    floodFillOptions.TypeIdsToConsiderPassable = ["minecraft:air"];
+    floodFillOptions.TypeIdsToAlwaysIncludeInResult = ["minecraft:oak_log"];
+    const blockFound = await new Promise<Block | null>(resolve => {
+      system.runJob(FindFirstBlock(resolve, floodFillOptions))
+    });
+    if (blockFound !== null){
+      console.warn(`Found oak log at: ${VectorUtils.GetAsString(blockFound.location)}`);
+    }else{
+      console.warn("Did not find oak log");
     }
   }
 });
+
+function *FindFirstBlock(
+  blockFoundResolve: (block: Block | null) => void, 
+  floodFillOptions: FloodFillIteratorOptions
+  ){
+  let blockThatWasFound: Block | null = null;
+  const floodFillIterator = new FloodFillIterator(floodFillOptions);
+  const startTimeMs = new Date().getTime();
+  console.warn("Start unix time: " + startTimeMs);
+  for (const block of floodFillIterator.IterateLocations()){
+      if (block !== null && block.isValid()){
+          if (block.typeId === "minecraft:oak_log"){
+              blockThatWasFound = block;
+              break;
+          }
+      }
+
+      // Important to yield to allow the MC engine to control how often this iterator runs!
+      yield;
+  }
+
+  const timeTook = (new Date().getTime()) - startTimeMs;
+  console.warn(`End. Time taken to search 64^3 blocks: ${timeTook}ms`);
+  return blockFoundResolve(blockThatWasFound);
+}
